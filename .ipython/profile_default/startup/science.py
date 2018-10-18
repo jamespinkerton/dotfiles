058146cdcb7845bac92bac1c5837ce2f9e0cde8c
James Pinkerton <jamescpinkerton@gmail.com>
2018-10-18 14:26:25 -0700

    Small fix to ipython init

--- .ipython/profile_default/startup/science.py
+++ .ipython/profile_default/startup/science.py
@@ -1,4 +1,8 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import sklearn
+try:
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+except:
+    print("Can't import plt, sns.")
