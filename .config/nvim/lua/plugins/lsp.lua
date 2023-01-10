f6edda8d1c28a98fc0f8e5fac9b8610445b4eb47
James Pinkerton <jpinkerton@latourtrading.com>
2023-01-10 17:30:36 -0500

    separate lsp into lsp and cmp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -98,7 +98,6 @@ return {
       },
       {
         'williamboman/mason-lspconfig.nvim',
-        -- dependencies = {'hrsh7th/cmp-nvim-lsp', 'nvim-telescope/telescope.nvim', 'williamboman/mason-lspconfig.nvim', "folke/neodev.nvim"},
       },
       -- Useful status updates for LSP
       {
@@ -116,41 +115,5 @@ return {
       },
     },
     config = startup_lsp,
-  },
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    config = function ()
-      local cmp = require('cmp')
-
-      cmp.setup {
-        mapping = cmp.mapping.preset.insert {
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          -- ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-        },
-      }
-    end,
-    dependencies = { 'hrsh7th/cmp-nvim-lsp'},
   },
 }
