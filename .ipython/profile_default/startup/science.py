0703d794b0c88fa0139949346f64479961b2b596
James Pinkerton <jpinkerton@latourtrading.com>
2023-02-18 10:22:11 -0500

    Add science.py

---
+++ .ipython/profile_default/startup/science.py
@@ -0,0 +1,9 @@
+from icecream import ic
+
+import numpy as np
+import polars as pl
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10,6))
+matplotlib.use('module://matplotlib-sixel')
