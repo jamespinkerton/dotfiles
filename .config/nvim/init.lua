44ec7832aa40565a5da13905817e8111b4dcb89f
James Pinkerton <james@tesujilabs.com>
2026-02-02 13:55:29 -0500

    fix deprecated code in init lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,7 +12,7 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 --
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
