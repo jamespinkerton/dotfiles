82c11db5c3d7e0ca736646f8711d883bed7c9cb7
James Pinkerton <james@tesujilabs.com>
2026-08-12 13:31:18 -0400

    fix wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,17 @@
 local wezterm = require 'wezterm'
 
 return {
-  -- use_fancy_tab_bar = false,
-  use_fancy_tab_bar = true,
-  window_frame = {
-    font_size = 7.0,
-  },
+  use_fancy_tab_bar = false,
+  -- use_fancy_tab_bar = true,
+  -- window_frame = {
+  --   font_size = 7.0,
+  -- },
   window_close_confirmation = 'NeverPrompt',
   font = wezterm.font_with_fallback {
     'Monaco Nerd Font Mono',
     'Monaco',
   },
+  audible_bell = "Disabled",
 
   font_size = 14.0,
   hide_tab_bar_if_only_one_tab = true,
