46b8417b8e5b84c9e6c9ff547ab064745facc6db
James Pinkerton <james@tesujilabs.com>
2026-08-13 12:50:51 -0400

    Simplify wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,29 +2,14 @@ local wezterm = require 'wezterm'
 
 return {
   use_fancy_tab_bar = false,
-  -- use_fancy_tab_bar = true,
-  -- window_frame = {
-  --   font_size = 7.0,
-  -- },
   window_close_confirmation = 'NeverPrompt',
   font = wezterm.font_with_fallback {
     'Monaco Nerd Font Mono',
     'Monaco',
   },
   audible_bell = "Disabled",
-
   font_size = 14.0,
   hide_tab_bar_if_only_one_tab = true,
-
   -- native_macos_fullscreen_mode = false,
   window_decorations = "RESIZE|TITLE",
-
-  -- keys = {
-  --   {
-  --     key = 'Enter',
-  --     mods = 'ALT',
-  --     action = wezterm.action.ToggleFullScreen,
-  --   },
-  -- },
-
 }
